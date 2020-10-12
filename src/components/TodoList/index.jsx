d0068191485479f96e11ea9cd2c8df6ece7adba2
Ahmad Sabry <ahmadsabry330@gmail.com>
2020-10-12 20:42:41 +0200

    Made Pages Dynamic & Added Todo's Label

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -4,22 +4,36 @@ import { connect } from "react-redux";
 import PropTypes from 'prop-types';
 import TodoItem from '../TodoItem';
 
-function TodoList({ todos }) {
+function TodoList({ completedTodos, notCompletedTodos }) {
   return (
     <div>
       {
-        todos?.map(todo => <TodoItem key={todo.id} {...todo} />)
+          notCompletedTodos.length > 0 && <>
+          <h3>Todos</h3>
+          {notCompletedTodos?.map(todo => <TodoItem key={todo.id} {...todo} />)}
+        </>
+      }
+      {
+        completedTodos.length > 0 && <>
+          <h3>Completed Todos</h3>
+          {completedTodos?.map(todo => <TodoItem key={todo.id} {...todo} />)}
+        </>
       }
     </div>
   );
 }
 
 TodoList.propTypes = {
-  todos: PropTypes.array
+  completedTodos: PropTypes.array,
+  notCompletedTodos: PropTypes.array
 };
 
-const mapStateToProps = state => ({
-  todos: state
-})
+const mapStateToProps = state => {
+  console.log("state", state)
+  return {
+    notCompletedTodos: state.filter(todo => !todo.completed),
+    completedTodos: state.filter(todo => todo.completed === true)
+  }
+}
 
 export default connect(mapStateToProps)(TodoList);
