08436de2f968562454a89cf1b76e45e3b90bb1a9
engmagdy87 <mohamed.magdy.abdelhamid@gmail.com>
2020-10-14 23:37:45 +0200

    add delete todo feature

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { todosAction } from "../../redux/actions";
 
-import styles from './styles.module.sass';
+import styles from "./styles.module.sass";
 
-function TodoItem() {
+function TodoItem({ todoText, todoId, deleteTodo }) {
   return (
     <div className={styles.card}>
-      <h2 className={styles.title}>Title</h2>
+      <h2 className={styles.title}>{todoText}</h2>
       <button className={styles.completeBtn}>Complete</button>
-      <button className={styles.deleteBtn}>Delete</button>
+      <button className={styles.deleteBtn} onClick={() => deleteTodo(todoId)}>
+        Delete
+      </button>
     </div>
   );
 }
 
-export default TodoItem;
+const mapDispatchToProps = (dispatch) => ({
+  deleteTodo: (id) => dispatch(todosAction.deleteTodo(id)),
+});
+
+export default connect(null, mapDispatchToProps)(TodoItem);
