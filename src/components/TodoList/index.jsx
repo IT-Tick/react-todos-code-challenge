cdd0ed962c15de7f6790bc88642e9f18f0255fbc
engmagdy87 <mohamed.magdy.abdelhamid@gmail.com>
2020-10-14 23:36:35 +0200

    list todos from store in TodoList

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import TodoItem from '../TodoItem';
+import React from "react";
+import { connect } from "react-redux";
+import TodoItem from "../TodoItem";
 
-function TodoList() {
-  return (
-    <div>
-      <TodoItem />
-      <TodoItem />
-    </div>
-  );
+function TodoList({ todos }) {
+  const renderTodos = todos.map((todo) => (
+    <TodoItem todoText={todo.title} key={todo.id} todoId={todo.id} />
+  ));
+  return <div>{renderTodos}</div>;
 }
+const mapStateToProps = (state) => ({
+  todos: state.todos.todosData,
+});
 
-export default TodoList;
+export default connect(mapStateToProps)(TodoList);
