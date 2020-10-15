bc28b00de9a72afed0054d41305ba84129099ce0
mostafafarag <mostafafarag140@gmail.com>
2020-10-15 18:06:54 +0200

    use seState Hook in form to keep input in local state

--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import styles from './styles.module.sass';
 
 function TodoForm() {
+  const [input, setInput] = useState('')
   return (
     <form>
       <input
         type="text"
         placeholder="What do you have to do?"
         className={styles.input}
+        value={input}
+        onChange={ e => setInput(e.target.value)}
       />
     </form>
   );
