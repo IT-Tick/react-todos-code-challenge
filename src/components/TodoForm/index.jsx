4979147872f149c11cd70893af7924af71273e73
mostafafarag <mostafafarag140@gmail.com>
2020-10-15 18:19:26 +0200

    make TodoForm able to add new Todo

--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,17 +1,33 @@
-import React, {useState} from 'react';
-
+import React, { useState, useContext } from 'react';
+import { Context } from '../../Redux/store'
 import styles from './styles.module.sass';
-
+import {getRandomNumber} from '../../utils/helperFunctions'
 function TodoForm() {
   const [input, setInput] = useState('')
+  const [state, dispatch] = useContext(Context)
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    dispatch(
+      {
+        type: 'ADD_TODO',
+        payload: {
+          id: getRandomNumber(state.todos),
+          title: input,
+          completed: false
+        }
+      }
+    )
+  }
+  
   return (
-    <form>
+    <form onSubmit = {handleSubmit}>
       <input
         type="text"
         placeholder="What do you have to do?"
         className={styles.input}
         value={input}
-        onChange={ e => setInput(e.target.value)}
+        onChange={e => setInput(e.target.value)}
       />
     </form>
   );
