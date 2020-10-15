53ae0075fa32ff8b7978cbb1e878be1f0a69acb5
mostafafarag <mostafafarag140@gmail.com>
2020-10-15 16:56:02 +0200

    use useReducer Hook by passing our created reducer and initial state to get our global state and dispatch to change that state

--- src/Redux/store.jsx
+++ src/Redux/store.jsx
@@ -1,4 +1,5 @@
-import React, { createContext } from 'react'
+import React, {useReducer, createContext } from 'react'
+import Reducer from './reducer'
 import {getRandomNumber} from '../utils/helperFunctions'
 
 
@@ -8,3 +9,14 @@ const initialState = {
     { id: getRandomNumber([]), title: 'task 2', completed: false }
   ]
 }
+
+const Store = ({ children }) => {
+  const [state, dispatch] = useReducer(Reducer, initialState)
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Store;
