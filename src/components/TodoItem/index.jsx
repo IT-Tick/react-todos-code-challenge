e64d34f355776adbcd14a6745d54d32684653b62
mostafafarag <mostafafarag140@gmail.com>
2020-10-15 18:32:18 +0200

    implement delete button functionality

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import styles from './styles.module.sass';
 
-function TodoItem({ todo }) {
+function TodoItem({ todo, dispatch }) {
+
+  const handleDelete = () => {
+    dispatch({
+      type: 'REMOVE_TODO',
+      payload: todo.id
+    })
+  }
   return (
     <div className={styles.card}>
       <h2 className={styles.title}>{todo.title}</h2>
       <button className={styles.completeBtn}>Complete</button>
-      <button className={styles.deleteBtn}>Delete</button>
+      <button className={styles.deleteBtn}
+      onClick = {handleDelete}>Delete</button>
     </div>
   );
 }
